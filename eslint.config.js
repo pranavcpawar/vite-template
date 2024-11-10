6ca59e4cc50e2e0c419fbbef070c6628a1fa5a80
pranavcpawar <pranavcpawar@outlook.com>
2024-11-11 00:58:00 +0530

    feat(setup): configure tailwindcss, prettier and eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,19 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import react from 'eslint-plugin-react'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import react from 'eslint-plugin-react';
+import prettier from 'eslint-plugin-prettier';
 
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.strictTypeChecked, ...tseslint.configs.stylisticTypeChecked],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.strictTypeChecked,
+      ...tseslint.configs.stylisticTypeChecked,
+    ],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       parserOptions: {
@@ -18,12 +23,19 @@ export default tseslint.config(
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
     plugins: {
       react,
+      prettier: prettier,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
     rules: {
+      ...prettier.configs.recommended.rules,
       ...react.configs.recommended.rules,
       ...react.configs['jsx-runtime'].rules,
       ...reactHooks.configs.recommended.rules,
@@ -33,4 +45,4 @@ export default tseslint.config(
       ],
     },
   },
-)
+);
--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
-import './App.css'
+import './App.css';
 
 function App() {
-
   return (
     <>
       <div>
-        <h1>App</h1>
+        <h1 className="text-3xl font-bold">App</h1>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
